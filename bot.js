95dbccb1cea445d07399d473eacb0a91589c60f3
SleepyDa <110206554+SleepyDa@users.noreply.github.com>
2022-07-30 22:32:13 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
+global.owner = ['50255162627',''] //owner number +27634090203
+global.wame = ['50255162627'] 
 global.pemilik = ['27634090203'] //owner number +27634090203
 global.premium = ['27634090203'] //premium number +27634090203
 global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.footer = 'SleepyBot'
+global.botnma = 'SleepyBot' //bot name
+global.botname = 'SleepyBot'
+global.ownernma = 'Sleepy' //owner name
+global.packname = 'SleepyBot' //sticker package name
+global.author = 'Pedroesnegro' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
